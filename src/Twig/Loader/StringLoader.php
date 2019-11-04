b9bae1a508fd13bae2b69ff3a20b6f72fbd9bfc2
Nikita Loges <git@loges.one>
2019-11-04 20:41:54 +0100

    cq

--- src/Twig/Loader/StringLoader.php
+++ src/Twig/Loader/StringLoader.php
@@ -4,40 +4,55 @@ declare(strict_types=1);
 
 namespace Shapecode\Twig\Loader;
 
+use Twig\Error\LoaderError;
 use Twig\Loader\LoaderInterface;
 use Twig\Source;
+use function md5;
 use function preg_match;
+use function sprintf;
 
 class StringLoader implements LoaderInterface
 {
     /**
      * @inheritDoc
      */
-    public function getSourceContext($name)
+    public function getSourceContext($name) : Source
     {
+        if (! $this->exists($name)) {
+            throw new LoaderError(sprintf('Template "%s" is not defined.', $name));
+        }
+
         return new Source($name, $name);
     }
 
     /**
      * @inheritdoc
      */
-    public function getCacheKey($name)
+    public function getCacheKey($name) : string
     {
-        return $name;
+        if (! $this->exists($name)) {
+            throw new LoaderError(sprintf('Template "%s" is not defined.', $name));
+        }
+
+        return md5($name);
     }
 
     /**
      * @inheritdoc
      */
-    public function isFresh($name, $time)
+    public function isFresh($name, $time) : bool
     {
+        if (! $this->exists($name)) {
+            throw new LoaderError(sprintf('Template "%s" is not defined.', $name));
+        }
+
         return true;
     }
 
     /**
      * @inheritDoc
      */
-    public function exists($name)
+    public function exists($name) : bool
     {
         return (bool) preg_match('/\s/', $name);
     }
---
+++ tests/Twig/Loader/StringLoaderTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shapecode\Tests\Twig\Loader;
+
+use PHPUnit\Framework\TestCase;
+use Shapecode\Twig\Loader\StringLoader;
+use Twig\Error\LoaderError;
+use function time;
+
+class StringLoaderTest extends TestCase
+{
+    public function testGetSourceContextWhenTemplateDoesNotExist() : void
+    {
+        $this->expectException(LoaderError::class);
+        $loader = new StringLoader();
+        $loader->getSourceContext('foo');
+    }
+
+    public function testGetCacheKey() : void
+    {
+        $loader = new StringLoader();
+        self::assertEquals('327b6f07435811239bc47e1544353273', $loader->getCacheKey('foo bar'));
+    }
+
+    public function testGetCacheKeyWhenTemplateDoesNotExist() : void
+    {
+        $this->expectException(LoaderError::class);
+        $loader = new StringLoader();
+        $loader->getCacheKey('foo');
+    }
+
+    public function testIsFresh() : void
+    {
+        $loader = new StringLoader();
+        self::assertTrue($loader->isFresh('foo bar', time()));
+    }
+
+    public function testIsFreshWhenTemplateDoesNotExist() : void
+    {
+        $this->expectException(LoaderError::class);
+
+        $loader = new StringLoader();
+        $loader->isFresh('foo', time());
+    }
+
+    public function testExists() : void
+    {
+        $loader = new StringLoader();
+        self::assertTrue($loader->exists('foo bar'));
+    }
+
+    public function testExistsFails() : void
+    {
+        $loader = new StringLoader();
+        self::assertFalse($loader->exists('foo'));
+    }
+}
